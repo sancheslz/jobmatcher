d6a4921ef67335b9ddfbeb6b29121b53c26d3211
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-11 18:41:24 -0300

    feat(company): add is_active (bool) to the model

---
+++ db/migrate/20210211213503_add_is_active_to_companies.rb
@@ -0,0 +1,5 @@
+class AddIsActiveToCompanies < ActiveRecord::Migration[6.0]
+  def change
+    add_column :companies, :is_active, :boolean, :default => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_185926) do
+ActiveRecord::Schema.define(version: 2021_02_11_213503) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_185926) do
     t.string "founded"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "is_active", default: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
