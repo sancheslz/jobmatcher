2af8c6be3a9508c128e2545ea4127f5022b70aca
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-15 23:01:00 -0300

    test(opportunity): add opportunity unit test validation

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -1,6 +1,16 @@
 class Opportunity < ApplicationRecord
+
+    before_save :add_date_if_empty
+
     validates :title, :description, :requirement, presence: true
     enum remote: { no: false, yes: true }
     enum level: { entry: 10, mid: 20, senior: 30 }
     belongs_to :company
+
+    private 
+    def add_date_if_empty
+        if self.expiration_date.nil?
+            self.expiration_date = 30.days.from_now
+        end
+    end
 end
--- spec/models/opportunity_spec.rb
+++ spec/models/opportunity_spec.rb
@@ -1,5 +1,88 @@
 require 'rails_helper'
 
-RSpec.describe Opportunity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Opportunity" do
+
+  context "#valid?" do
+
+    it "title is required" do
+      # Arrange
+
+      # Act
+      opportunity = Opportunity.new
+      opportunity.valid?
+
+      # Assert
+      expect(opportunity.errors[:title]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "description is required" do
+      # Arrange
+
+      # Act
+      opportunity = Opportunity.new
+      opportunity.valid?
+
+      # Assert
+      expect(opportunity.errors[:description]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "requirement is required" do
+      # Arrange
+
+      # Act
+      opportunity = Opportunity.new
+      opportunity.valid?
+
+      # Assert
+      expect(opportunity.errors[:requirement]).to include(I18n.t('errors.messages.blank'))
+
+    end
+
+  end
+  
+  context "#add_date_if_empty" do
+    
+    it "user fill date" do
+      # Arrange
+      Company.create!(
+        name: 'Campus Code',
+        cnpj: '1234',
+        website: 'www.campuscode.com.br'
+      )
+      # Act
+      opportunity = Opportunity.create!(
+        company: Company.last,
+        title: 'Desenvolvedor Rails',
+        description: 'Conhecimento de Rails e Angular',
+        requirement: 'Experiência com Rails 6',
+        expiration_date: Time.now.strftime('%d/%m/%Y')
+      )
+      
+      # Assert
+      expect(opportunity.reload.expiration_date.strftime('%d/%m/%Y')).to eq(Time.now.strftime('%d/%m/%Y'))
+    end
+    
+    it "default 30 days if empty" do
+      # Arrange
+      Company.create!(
+        name: 'Campus Code',
+        cnpj: '1234',
+        website: 'www.campuscode.com.br'
+      )
+      # Act
+      opportunity = Opportunity.create!(
+        company: Company.last,
+        title: 'Desenvolvedor Rails',
+        description: 'Conhecimento de Rails e Angular',
+        requirement: 'Experiência com Rails 6'
+      )
+      
+      # Assert
+      expect(opportunity.reload.expiration_date.strftime('%d/%m/%Y')).to eq(30.days.from_now.strftime('%d/%m/%Y'))
+    end
+    
+
+  end
+  
+  
 end
