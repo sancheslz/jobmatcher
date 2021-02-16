07e684217a7bbd725a5d18807645a765e8118f85
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-15 22:42:48 -0300

    test(company): company unit test validation

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,103 @@
 require 'rails_helper'
 
-RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "a specification" do
+
+  context "valid?" do
+
+    it "name is required" do
+      # Arrange
+
+      # Act
+      company = Company.new
+      company.valid?
+
+      # Assert
+      expect(company.errors[:name]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "cnpj is required" do
+      # Arrange
+
+      # Act
+      company = Company.new
+      company.valid?
+
+      # Assert
+      expect(company.errors[:cnpj]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "website is required" do
+      # Arrange
+
+      # Act
+      company = Company.new
+      company.valid?
+
+      # Assert
+      expect(company.errors[:website]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "cnpj is unique" do
+      # Arrange
+      Company.create!(
+        name: 'Campus Code',
+        cnpj: '1234',
+        website: 'www.campuscode.com.br'
+      )
+
+      # Act
+      company = Company.new(
+        name: 'Rebase',
+        cnpj: '1234',
+        website: 'www.rebase.com.br'
+      )
+      company.valid?
+
+      # Assert
+      expect(company.errors[:cnpj]).to include(I18n.t('errors.messages.taken'))
+    end
+    
+  end
+
+  context "#active?" do
+
+    it "returns is_active attribute" do
+      # Arrange
+      
+      # Act
+      company = Company.new(
+        name: 'Rebase',
+        cnpj: '1234',
+        website: 'www.rebase.com.br'
+      )
+
+      # Assert
+      expect(company.active?).to eq(company.is_active)
+
+    end
+        
+  end
+  
+
+  context "#change_state!" do
+    it "description" do
+      
+      # Arrange
+      company = Company.new(
+        name: 'Rebase',
+        cnpj: '1234',
+        website: 'www.rebase.com.br'
+      )
+  
+      # Act
+      initial_state = company.active?
+      company.change_state!
+  
+      # Assert
+      expect(company.active?).not_to eq(initial_state)
+      
+    end
+    
+  end
+  
 end
