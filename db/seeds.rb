337fbe3fc6ff6e29d032f216ccc20d1a51fe5435
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-15 21:55:29 -0300

    test(seed): adding opportunity to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,37 @@
 Company.create!(
     name: 'Campus Code',
     cnpj: '1233456',
-    address: 'My address',
-    number: '1234',
-    complement: 'BLC B',
-    neighborhood: 'Paulista',
+    address: 'Alameda Santos',
+    number: '1293',
+    complement: 'Conj 73',
+    neighborhood: 'Jardim Paulista',
     city: 'São Paulo',
     state: 'SP',
-    postal_code: '11345-345',
+    postal_code: '11345-111',
     website: 'www.campuscode.com',
     founded:  '2010',
 )
 
-Technology.create!(name: 'Rails')
-Technology.create!(name: 'Ruby')
-Technology.create!(name: 'Git')
+Technology.create!(
+    name: 'Rails'
+)
+
+Technology.create!(
+    name: 'Ruby'
+)
+
+Technology.create!(
+    name: 'Git'
+)
+
+
+Opportunity.create!(
+    title: 'Desenvolvedor Rails',
+    description: 'Deverá desenvolver aplicações Rails com entrega e consumo de api',
+    requirement: 'Conhecimento em ruby e rails. Desejável conhecimento em React',
+    expiration_date: 30.days.from_now,
+    position_quantity: 1,
+    salary: 2500.00,
+    remote: :true,
+    level: :entry,
+)
