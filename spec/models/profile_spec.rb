2aba23f411c0d6b7566e9ecdadb8c42e2a4a6268
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-15 22:22:24 -0300

    test(profile): profile unit test validation

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,79 @@
 require 'rails_helper'
 
-RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Profile do
+  
+  context "valid?" do
+
+    it "name is required" do
+      # Arrange
+
+      # Act
+      profile = Profile.new
+      profile.valid?
+
+      # Assert
+      expect(profile.errors[:name]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "cpf is required" do
+      # Arrange
+
+      # Act
+      profile = Profile.new
+      profile.valid?
+
+      # Assert
+      expect(profile.errors[:cpf]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "cpf is unique" do
+      # Arrange
+      Profile.create!(
+        name: 'Bob Smith',
+        cpf: '123'
+      )
+
+      # Act
+      profile = Profile.new(
+        cpf: '123'
+      )
+      profile.valid?
+
+      # Assert
+      expect(profile.errors[:cpf]).to include(I18n.t('errors.messages.taken'))
+    end
+
+    it "active as default" do
+      # Arrange
+      
+      # Act
+      profile = Profile.create!(
+        name: 'Bob Smith',
+        cpf: '123'
+      )
+
+      # Assert
+      expect(profile.reload.is_active).to eq(true)
+    end
+    
+  end
+
+  context "#active?" do
+
+    it "returns is_active attribute" do
+      # Arrange
+
+      # Act
+      profile = Profile.create!(
+        name: 'Bob Smith',
+        cpf: '123'
+      )
+
+      # Assert
+      expect(profile.reload.active?).to eq(profile.is_active)
+    end
+    
+    
+  end  
+
 end
