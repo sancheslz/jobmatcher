f6743d19fab120d5e977198b1e1e36c028841cd8
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-16 16:02:21 -0300

    chore(gem): install devise to project

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,6 @@ Rails.application.configure do
 
   # Active Storage
   config.active_storage.service = :local
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
