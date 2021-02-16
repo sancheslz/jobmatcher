aea93a528495e444325cfb713c3d44dfda4884a5
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-15 22:29:16 -0300

    test(technology): technology unit test validation

--- spec/models/technology_spec.rb
+++ spec/models/technology_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe Technology, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Technology" do
+
+  context "valid?" do
+    it "name is required" do
+      # Arrange
+
+      # Act
+      technology = Technology.new
+      technology.valid?
+
+      # Assert
+      expect(technology.errors[:name]).to include(I18n.t('errors.messages.blank'))
+    end
+
+    it "name is unique" do
+      # Arrange
+      Technology.create!(
+        name: 'Ruby'
+      )
+
+      # Act
+      technology = Technology.new(
+        name: 'Ruby'
+      )
+      technology.valid?
+
+      # Assert
+      expect(technology.errors[:name]).to include(I18n.t('errors.messages.taken'))
+    end
+
+    it "name is unique and case insensitive" do
+      # Arrange
+      Technology.create!(
+        name: 'Ruby'
+      )
+
+      # Act
+      technology = Technology.new(
+        name: 'ruby'
+      )
+      technology.valid?
+
+      # Assert
+      expect(technology.errors[:name]).to include(I18n.t('errors.messages.taken'))
+    end
+    
+  end
+  
 end
