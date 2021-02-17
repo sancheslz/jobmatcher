7fe065f874bc9dccd71bff366e1d90de5beed2d0
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-17 18:31:43 -0300

    test(profile): write unit test to #set_role! method

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -88,7 +88,82 @@ describe Profile do
       expect(profile.reload.active?).to eq(profile.is_active)
     end
     
-    
   end  
+
+  context "#set_role!" do
+    
+    it "regular user" do
+      # Arrange
+      user = User.create!(
+        email: 'user@gmail.com',
+        password: '123456'
+      )
+      # Act
+      profile = Profile.create!(
+        name: 'Bob Smith',
+        cpf: '123',
+        user: user
+      )
+
+      profile.set_role!(user.reload)
+      profile.save
+
+      # Assert
+      expect(profile.reload.role).to eq('regular')
+    end
+    
+    it "first business user" do
+      # Arrange
+      user = User.create!(
+        email: 'user@rebase.com',
+        password: '123456'
+      )
+      # Act
+      profile = Profile.create!(
+        name: 'Bob Smith',
+        cpf: '123',
+        user: user
+      )
+
+      profile.set_role!(user.reload)
+      profile.save
+
+      # Assert
+      expect(profile.reload.role).to eq('admin')
+    end
+    
+    it "other business user" do
+      # Arrange
+      first_user = User.create!(
+        email: 'user@rebase.com',
+        password: '123456'
+      )
+      Profile.create!(
+        name: 'Bob Smith',
+        cpf: '123',
+        user: first_user,
+        role: 'admin'
+      )
+
+      user = User.create!(
+        email: 'other@rebase.com',
+        password: '123456'
+      )
+
+      # Act
+      profile = Profile.create!(
+        name: 'Bob Smith',
+        cpf: '124',
+        user: user
+      )
+
+      profile.set_role!(user.reload)
+      profile.save
+
+      # Assert
+      expect(profile.reload.role).to eq('business')
+    end
+    
+  end
 
 end
