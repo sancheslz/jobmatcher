6fb2c9ab989f721cceb2c9fdfe27ac9c08521cc6
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-19 11:27:58 -0300

    test(company): inactive companies can't be edited

--- spec/features/admin_edit_company_spec.rb
+++ spec/features/admin_edit_company_spec.rb
@@ -222,6 +222,126 @@ feature('Admin edit company') do
         expect(page).to have_content(I18n.t('errors.messages.taken')) 
     end
 
+    scenario('can\'t edit an inactive company') do
+        # Arrange
+        other_company = Company.create!(
+            name: 'MoemaSoft',
+            cnpj: '815.916.474/1594-96',
+            address: 'Rua São Sebastiao',
+            number: '8',
+            complement: 'Park',
+            city: 'Caxias',
+            state: 'Alagoas',
+            postal_code: '56170',
+            website: 'www.moemasoft.com',
+            founded:'1983',
+        )
+
+        user = User.create!(
+            email: 'graciliana.novaes@cdev.com',
+            password: 'brownbird755'
+        )
+
+        profile = Profile.create!(
+            name: 'Graciliana Novaes',
+            cpf: '388.586.242-26',
+            address: 'Rua Quatro',
+            number: '8229',
+            complement: '-', 
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '75674',
+            role: 'admin',
+            user: user
+        )
+
+        company = Company.create!(
+            name: 'Continuous Development',
+            cnpj: '832.538.794/9328-87',
+            address: 'Rio Grande do Sul',
+            number: '347',
+            complement: 'Center',
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '85390',
+            website: 'www.cdev.com',
+            founded:'2009',
+            is_active: false
+        )
+
+        CompanyProfile.create!( profile: profile, company: company )
+        
+        login_as user
+
+        # Act
+        visit root_path
+        within('nav') do
+            click_on 'Menu'
+            click_on I18n.t('views.companies.mine')
+        end
+
+        # Assert
+        expect(page).not_to have_link(I18n.t('views.main.edit'))
+    end
+
+    scenario('can\'t access edit url of inactive companies') do
+        # Arrange
+        other_company = Company.create!(
+            name: 'MoemaSoft',
+            cnpj: '815.916.474/1594-96',
+            address: 'Rua São Sebastiao',
+            number: '8',
+            complement: 'Park',
+            city: 'Caxias',
+            state: 'Alagoas',
+            postal_code: '56170',
+            website: 'www.moemasoft.com',
+            founded:'1983',
+        )
+
+        user = User.create!(
+            email: 'graciliana.novaes@cdev.com',
+            password: 'brownbird755'
+        )
+
+        profile = Profile.create!(
+            name: 'Graciliana Novaes',
+            cpf: '388.586.242-26',
+            address: 'Rua Quatro',
+            number: '8229',
+            complement: '-', 
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '75674',
+            role: 'admin',
+            user: user
+        )
+
+        company = Company.create!(
+            name: 'Continuous Development',
+            cnpj: '832.538.794/9328-87',
+            address: 'Rio Grande do Sul',
+            number: '347',
+            complement: 'Center',
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '85390',
+            website: 'www.cdev.com',
+            founded:'2009',
+            is_active: false
+        )
+
+        CompanyProfile.create!( profile: profile, company: company )
+        
+        login_as user
+
+        # Act
+        visit edit_company_path(company.reload)
+
+        # Assert
+        expect(current_path).to eq(root_path)
+    end
+
     scenario('can set a logo') do
         # Arrange
         other_company = Company.create!(
