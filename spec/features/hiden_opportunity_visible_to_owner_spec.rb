71c7ad27ae0fb50dd0ea287bc3478f692b5bd194
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-23 15:32:54 -0300

    test(company): hidden opportunities are visible just to the owner

---
+++ spec/features/hiden_opportunity_visible_to_owner_spec.rb
@@ -0,0 +1,145 @@
+require 'rails_helper'
+
+feature('Hidden opportunities are visible to owner') do
+    
+    scenario('owner can see') do
+        # Arrange 
+        user = User.create!(
+            email: 'graciliana.novaes@cdev.com',
+            password: 'brownbird755'
+        )
+    
+        profile = Profile.create!(
+            name: 'Graciliana Novaes',
+            cpf: '388.586.242-26',
+            address: 'Rua Quatro',
+            number: '8229',
+            complement: '-', 
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '75674',
+            role: 'admin',
+            user: user
+        )
+
+        company = Company.create!(
+            name: 'Continuous Development',
+            cnpj: '832.538.794/9328-87',
+            address: 'Rio Grande do Sul',
+            number: '347',
+            complement: 'Center',
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '85390',
+            website: 'www.cdev.com',
+            founded:'2009',
+        )
+    
+        opportunity = Opportunity.create!(
+            title: "Desenvolvedor(a) Front End",
+            description: %{Você será responsável por desenvolver interfaces web a partir de especificações realizadas pelo time de design primariamente utilizando ReactJs mas também atendendo demandas de desenvolvimento e manutenção em sistemas desenvolvidos com React Native.},
+            requirement: %{Grande conhecimento em desenvolvimento ReactJs | Habilidade para desenvolver em Typescript | Experiência em desenvolvimento React Native | Capacidade de escrever código limpo e bem documentado | Conhecimento em Hooks, TDD, Clean Architecture, SOLID},
+            position_quantity: 1,
+            expiration_date: 30.days.from_now,
+            salary: 8000,
+            remote: true,
+            level: :mid,
+            company: company,
+            is_visible: false,
+        )
+
+        CompanyProfile.create!( profile: profile, company: company )
+
+        login_as user 
+
+        # Act
+        visit root_path 
+        within('nav') do
+            click_on 'Menu'
+            click_on I18n.t('views.companies.mine')
+        end
+
+        # Assert
+        within('div.card-body.border-danger') do 
+            expect(page).to have_content(opportunity.title) 
+        end
+    end
+    
+    scenario('others can\'t see') do
+        # Arrange 
+        owner_user = User.create!(
+            email: 'graciliana.novaes@cdev.com',
+            password: 'brownbird755'
+        )
+    
+        owner_profile = Profile.create!(
+            name: 'Graciliana Novaes',
+            cpf: '388.586.242-26',
+            address: 'Rua Quatro',
+            number: '8229',
+            complement: '-', 
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '75674',
+            role: 'admin',
+            user: owner_user
+        )
+
+        company = Company.create!(
+            name: 'Continuous Development',
+            cnpj: '832.538.794/9328-87',
+            address: 'Rio Grande do Sul',
+            number: '347',
+            complement: 'Center',
+            city: 'Resende',
+            state: 'Roraima', 
+            postal_code: '85390',
+            website: 'www.cdev.com',
+            founded:'2009',
+        )
+    
+        opportunity = Opportunity.create!(
+            title: "Desenvolvedor(a) Front End",
+            description: %{Você será responsável por desenvolver interfaces web a partir de especificações realizadas pelo time de design primariamente utilizando ReactJs mas também atendendo demandas de desenvolvimento e manutenção em sistemas desenvolvidos com React Native.},
+            requirement: %{Grande conhecimento em desenvolvimento ReactJs | Habilidade para desenvolver em Typescript | Experiência em desenvolvimento React Native | Capacidade de escrever código limpo e bem documentado | Conhecimento em Hooks, TDD, Clean Architecture, SOLID},
+            position_quantity: 1,
+            expiration_date: 30.days.from_now,
+            salary: 8000,
+            remote: true,
+            level: :mid,
+            company: company,
+            is_visible: false,
+        )
+
+        user = User.create!(
+            email: 'felismino.daconceicao@gmail.com',
+            password: 'yellowtiger502'
+        )
+
+        Profile.create!(
+            name: 'Felismino da Conceição',
+            cpf: '175.113.263-01',
+            address: 'Rua Treze de Maio',
+            number: '7971',
+            complement: 'C12', 
+            city: 'Nossa Senhora do Socorro',
+            state: 'Espírito Santo', 
+            postal_code: '84373',
+            role: 'regular',
+            user: user
+        )
+
+        CompanyProfile.create!( profile: owner_profile, company: company )
+
+        login_as user 
+
+        # Act
+        visit company_path(company)
+
+        # Assert
+        within('div.card-body.border-danger') do 
+            expect(page).not_to have_content(opportunity.title) 
+        end
+    end
+
+end
