6d9f2f7f78ffa18b88af5b63862579499e62af05
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-23 16:08:37 -0300

    test(submission): candidate can apply from opportunity details

--- spec/features/candidate_can_apply_spec.rb
+++ spec/features/candidate_can_apply_spec.rb
@@ -39,225 +39,307 @@ feature('Candidate can apply to an opportunity') do
     
     end
 
-    scenario('see the apply button on opportunity card') do
-        # Arrange
-        user = User.create!(
-            email: 'felismino.daconceicao@gmail.com',
-            password: 'yellowtiger502'
-        )
+    context('home page') do
 
-        profile = Profile.create!(
-            name: 'Felismino da Conceição',
-            cpf: '175.113.263-01',
-            address: 'Rua Treze de Maio',
-            number: '7971',
-            complement: 'C12', 
-            city: 'Nossa Senhora do Socorro',
-            state: 'Espírito Santo', 
-            postal_code: '84373',
-            role: 'regular',
-            user: user
-        )
+        scenario('see the apply button on opportunity card') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
 
-        login_as user
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
 
-        # Act
-        visit root_path
-        
-        # Assert
-        @opportunity.reload
-        within("div#opportunity_#{@opportunity.id}") do
-            expect(page).to have_link(I18n.t('views.submissions.apply'))
+            login_as user
+
+            # Act
+            visit root_path
+            
+            # Assert
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                expect(page).to have_link(I18n.t('views.submissions.apply'))
+            end
+        end
+
+        scenario('goes to the application path') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
+
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
+
+            login_as user
+
+            # Act
+            visit root_path
+            
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.submissions.apply')
+            end
+            
+            # Assert
+            expect(current_path).to eq(new_submission_path)
+        end
+
+        scenario('see opportunity details') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
+
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
+
+            login_as user
+
+            # Act
+            visit root_path
+
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.submissions.apply')
+            end
+            
+            # Assert
+            expect(page).to have_content(@opportunity.title)
+            expect(page).to have_content(@opportunity.description)
+            expect(page).to have_content(@opportunity.requirement)
+            expect(page).to have_content(@opportunity.position_quantity)
+            expect(page).to have_content(@opportunity.company.name)
+        end
+
+        scenario('have fields to fill') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
+
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
+
+            login_as user
+
+            # Act
+            visit root_path
+
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.submissions.apply')
+            end
+
+            # Assert
+            within('form') do
+                expect(page).to have_field(I18n.t('activerecord.attributes.submission.presentation')) 
+                expect(page).to have_field(I18n.t('activerecord.attributes.submission.wage_claim')) 
+                expect(page).to have_field(I18n.t('activerecord.attributes.submission.note')) 
+            end
+        end
+
+        scenario('some are required') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
+
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
+
+            login_as user
+
+            # Act
+            visit root_path
+
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.submissions.apply')
+            end
+            
+            within('form') do
+                click_on I18n.t('views.submissions.apply')
+            end
+
+            # Assert
+            expect(page).to have_content(I18n.t('errors.messages.blank'), count: 2)
+        end
+
+        scenario('create with success') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
+
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
+
+            login_as user
+
+            # Act
+            visit root_path
+
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.submissions.apply')
+            end
+            
+            within('form') do
+                fill_in I18n.t('activerecord.attributes.submission.presentation'), with: %{Tenho 10 anos de experiência como Engenheiro de Produção em uma empresa de grande porte, atuei no controle logístico, de produção e gerência. Estou em transição de carreira para a área de desenvolvimento de software. Tenho experiência com JavaScript, React, Angular, Vue e PHP}
+                fill_in I18n.t('activerecord.attributes.submission.wage_claim'), with: 7000
+                click_on I18n.t('views.submissions.apply')
+            end
+
+            # Assert
+            expect(current_path).to eq(opportunity_path(@opportunity))
+            expect(page).to have_content(I18n.t('views.submissions.applied'))
         end
     end
 
-    scenario('goes to the application path') do
-        # Arrange
-        user = User.create!(
-            email: 'felismino.daconceicao@gmail.com',
-            password: 'yellowtiger502'
-        )
+    context('opportunity details') do
 
-        profile = Profile.create!(
-            name: 'Felismino da Conceição',
-            cpf: '175.113.263-01',
-            address: 'Rua Treze de Maio',
-            number: '7971',
-            complement: 'C12', 
-            city: 'Nossa Senhora do Socorro',
-            state: 'Espírito Santo', 
-            postal_code: '84373',
-            role: 'regular',
-            user: user
-        )
+        scenario('see button to apply in opportunity details') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
 
-        login_as user
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
 
-        # Act
-        visit root_path
-        
-        @opportunity.reload
-        within("div#opportunity_#{@opportunity.id}") do
+            login_as user
+
+            # Act
+            visit root_path
+
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.home.more')
+            end
+
+            # Assert
+            expect(page).to have_content(I18n.t('views.submissions.apply'))
+        end
+
+        scenario('can apply in opportunity details') do
+            # Arrange
+            user = User.create!(
+                email: 'felismino.daconceicao@gmail.com',
+                password: 'yellowtiger502'
+            )
+
+            profile = Profile.create!(
+                name: 'Felismino da Conceição',
+                cpf: '175.113.263-01',
+                address: 'Rua Treze de Maio',
+                number: '7971',
+                complement: 'C12', 
+                city: 'Nossa Senhora do Socorro',
+                state: 'Espírito Santo', 
+                postal_code: '84373',
+                role: 'regular',
+                user: user
+            )
+
+            login_as user
+
+            # Act
+            visit root_path
+
+            @opportunity.reload
+            within("div#opportunity_#{@opportunity.id}") do
+                click_on I18n.t('views.home.more')
+            end
             click_on I18n.t('views.submissions.apply')
+            
+            within('form') do
+                fill_in I18n.t('activerecord.attributes.submission.presentation'), with: %{Tenho 10 anos de experiência como Engenheiro de Produção em uma empresa de grande porte, atuei no controle logístico, de produção e gerência. Estou em transição de carreira para a área de desenvolvimento de software. Tenho experiência com JavaScript, React, Angular, Vue e PHP}
+                fill_in I18n.t('activerecord.attributes.submission.wage_claim'), with: 7000
+                click_on I18n.t('views.submissions.apply')
+            end
+
+            # Assert
+            expect(current_path).to eq(opportunity_path(@opportunity))
+            expect(page).to have_content(I18n.t('views.submissions.applied'))
         end
-        
-        # Assert
-        expect(current_path).to eq(new_submission_path)
-    end
-
-    scenario('see opportunity details') do
-        # Arrange
-        user = User.create!(
-            email: 'felismino.daconceicao@gmail.com',
-            password: 'yellowtiger502'
-        )
-
-        profile = Profile.create!(
-            name: 'Felismino da Conceição',
-            cpf: '175.113.263-01',
-            address: 'Rua Treze de Maio',
-            number: '7971',
-            complement: 'C12', 
-            city: 'Nossa Senhora do Socorro',
-            state: 'Espírito Santo', 
-            postal_code: '84373',
-            role: 'regular',
-            user: user
-        )
-
-        login_as user
-
-        # Act
-        visit root_path
-
-        @opportunity.reload
-        within("div#opportunity_#{@opportunity.id}") do
-            click_on I18n.t('views.submissions.apply')
-        end
-        
-        # Assert
-        expect(page).to have_content(@opportunity.title)
-        expect(page).to have_content(@opportunity.description)
-        expect(page).to have_content(@opportunity.requirement)
-        expect(page).to have_content(@opportunity.position_quantity)
-        expect(page).to have_content(@opportunity.company.name)
-    end
-
-    scenario('have fields to fill') do
-        # Arrange
-        user = User.create!(
-            email: 'felismino.daconceicao@gmail.com',
-            password: 'yellowtiger502'
-        )
-
-        profile = Profile.create!(
-            name: 'Felismino da Conceição',
-            cpf: '175.113.263-01',
-            address: 'Rua Treze de Maio',
-            number: '7971',
-            complement: 'C12', 
-            city: 'Nossa Senhora do Socorro',
-            state: 'Espírito Santo', 
-            postal_code: '84373',
-            role: 'regular',
-            user: user
-        )
-
-        login_as user
-
-        # Act
-        visit root_path
-
-        @opportunity.reload
-        within("div#opportunity_#{@opportunity.id}") do
-            click_on I18n.t('views.submissions.apply')
-        end
-
-        # Assert
-        within('form') do
-            expect(page).to have_field(I18n.t('activerecord.attributes.submission.presentation')) 
-            expect(page).to have_field(I18n.t('activerecord.attributes.submission.wage_claim')) 
-            expect(page).to have_field(I18n.t('activerecord.attributes.submission.note')) 
-        end
-    end
-
-    scenario('some are required') do
-        # Arrange
-        user = User.create!(
-            email: 'felismino.daconceicao@gmail.com',
-            password: 'yellowtiger502'
-        )
-
-        profile = Profile.create!(
-            name: 'Felismino da Conceição',
-            cpf: '175.113.263-01',
-            address: 'Rua Treze de Maio',
-            number: '7971',
-            complement: 'C12', 
-            city: 'Nossa Senhora do Socorro',
-            state: 'Espírito Santo', 
-            postal_code: '84373',
-            role: 'regular',
-            user: user
-        )
-
-        login_as user
-
-        # Act
-        visit root_path
-
-        @opportunity.reload
-        within("div#opportunity_#{@opportunity.id}") do
-            click_on I18n.t('views.submissions.apply')
-        end
-        
-        within('form') do
-            click_on I18n.t('views.submissions.apply')
-        end
-
-        # Assert
-        expect(page).to have_content(I18n.t('errors.messages.blank'), count: 2)
-    end
-
-    scenario('create with success') do
-        # Arrange
-        user = User.create!(
-            email: 'felismino.daconceicao@gmail.com',
-            password: 'yellowtiger502'
-        )
-
-        profile = Profile.create!(
-            name: 'Felismino da Conceição',
-            cpf: '175.113.263-01',
-            address: 'Rua Treze de Maio',
-            number: '7971',
-            complement: 'C12', 
-            city: 'Nossa Senhora do Socorro',
-            state: 'Espírito Santo', 
-            postal_code: '84373',
-            role: 'regular',
-            user: user
-        )
-
-        login_as user
-
-        # Act
-        visit root_path
-
-        @opportunity.reload
-        within("div#opportunity_#{@opportunity.id}") do
-            click_on I18n.t('views.submissions.apply')
-        end
-        
-        within('form') do
-            fill_in I18n.t('activerecord.attributes.submission.presentation'), with: %{Tenho 10 anos de experiência como Engenheiro de Produção em uma empresa de grande porte, atuei no controle logístico, de produção e gerência. Estou em transição de carreira para a área de desenvolvimento de software. Tenho experiência com JavaScript, React, Angular, Vue e PHP}
-            fill_in I18n.t('activerecord.attributes.submission.wage_claim'), with: 7000
-            click_on I18n.t('views.submissions.apply')
-        end
-
-        # Assert
-        expect(current_path).to eq(opportunity_path(@opportunity))
-        expect(page).to have_content(I18n.t('views.submissions.applied'))
     end
 
 end
